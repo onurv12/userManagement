59085c9e200d6548a37a0560d7ffaaa977f03537
Tobias Mücksch <tobias.muecksch@gmail.com>
2014-05-31 16:52:03 +0200

    Refactor atLeast function
    to respect the suggested changes boom1992.
    
    See: https://github.com/Paperdreamer/userManagement/pull/2#discussion-diff-13260555

--- projectPermission.class.php
+++ projectPermission.class.php
@@ -3,11 +3,11 @@
 	abstract class projectPermission {
 		private static $permissions = Array("Director", "Supervisor", "Artist", "Observer");
 
-		public static function atLeast ($minRole, isRole) {
-			$minRole = array_search($minRole, $this->permissions);
-			$isRole = array_search($minRole, $this->permissions);
+		public static function atLeast ($atLeastRole, $currentRole) {
+			$atLeastRole = array_search($atLeastRole, $this->permissions);
+			$currentRole = array_search($currentRole, $this->permissions);
 			
-			return $isRole <= $minRole;
+			return $currentRole <= $atLeastRole;
 		}
 	}
 ?>
