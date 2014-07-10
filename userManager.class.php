cc18763e6ea77570585fc9b8ab3c402b8d473c02
onurv12 <m.onur_vural@hotmail.com>
2014-07-10 14:26:13 +0200

    Change getAllActiveUsers() to return also Deleteable

--- userManager.class.php
+++ userManager.class.php
@@ -114,7 +114,7 @@
 		}
 		
 		public function getAllActiveUsers() {
-			return $this->DB->getList("SELECT ID, Name, Fullname, Email, GravatarEmail, (Admins.Deleteable IS NOT NULL) AS Admin FROM " . USER_TABLE . " LEFT JOIN " . ADMIN_TABLE . " ON Users.ID = Admins.UserID WHERE Suspended=0");
+			return $this->DB->getList("SELECT ID, Name, Fullname, Email, GravatarEmail, Deleteable, (Admins.Deleteable IS NOT NULL) AS Admin FROM " . USER_TABLE . " LEFT JOIN " . ADMIN_TABLE . " ON Users.ID = Admins.UserID WHERE Suspended=0");
 		}
 		
 		public function getAllSuspendedUsers() {
